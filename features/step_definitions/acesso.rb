8722c50c07a1fb33692ac93a1195efdfad8a54e9
“rafaelribon <ribon.rafael@gmail.com>
2018-05-14 15:42:19 -0300

    ajustes no page objects

--- features/step_definitions/acesso.rb
+++ features/step_definitions/acesso.rb
@@ -2,17 +2,16 @@
 
  
 Dado("que eu acesse o site do heroku") do
-  visit'https://ninjainvoices.herokuapp.com'
+  visit 'https://ninjainvoices.herokuapp.com'
 end
 
 Quando("eu realizar um cadastro novo") do
   find(:css, '#register').click
-   @password = Faker::Base.numerify('Capybara####').to_s
-   @name = Faker::StarWars.character
+  @name = Faker::StarWars.character
    acesso = Acesso.new
    acesso.nome.set(@name)
-   acesso.password.set(@password)
    acesso.email.set(Faker::Internet.email('teste'))
+   acesso.senha.set(102030)
    find(:css, 'button[class ="btn btn-lg btn-primary"]').click
   end
 
--- features/support/Acesso_po.rb
+++ features/support/Acesso_po.rb
@@ -1,10 +1,16 @@
 # encoding: utf-8
 #!/usr/bin/env ruby
 
-class Acesso < SitePrism: :Page
-    element :nome, "input[id='register_name']"
+class Acesso < SitePrism::Page
+
+    element :nome,  "input[id='register_name']"
     element :email, "input[id='register_email']"
     element :senha, "input[id='register_password']"
+     
 end 
+
+
+
+
 
     
